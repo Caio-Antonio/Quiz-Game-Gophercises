ee774e3d7fb51fa4d8f7cd9413a86a7697514076
Caio Almeida <caioantonio0903@gmail.com>
2023-10-28 03:19:00 -0300

    adding comments

--- main.go
+++ main.go
@@ -12,10 +12,12 @@ func main () {
 	var cnt = 0
 	var icnt = 0
 	var espaco string
+	//Opening CSV
 	file, _ := os.Open("problems.csv")
 	defer file.Close()
 	reader := csv.NewReader(file)
 	data,_ := reader.ReadAll()
+	//Initializing the timer
 	fmt.Printf("Digite t para iniciar o timer\n")
 	fmt.Scanf("%s", &espaco)
 	if espaco == "t" {
@@ -23,6 +25,7 @@ func main () {
 		for _, value := range data {
 			icnt++
 			fmt.Printf("%s = ", value[0])
+			//Setting the the timer to init alongside the loop
 			time.AfterFunc(30 * time.Second, func (){
 				fmt.Println()
 				fmt.Printf("Voce acertou %d de %d", cnt, icnt )
